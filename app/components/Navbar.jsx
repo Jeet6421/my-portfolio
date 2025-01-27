983cf6758c875c3e3282c49a8c9f68168a08b3f4
Jeet6421 <111728525+Jeet6421@users.noreply.github.com>
2025-01-28 00:06:20 +0530

    fix: update Navbar link from 'Resume' to 'Portfolio'

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -138,7 +138,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
           </li>
           <li>
             <a className="font-Ovo" onClick={closeMenu} href="#resume">
-              Resume
+              Portfolio
             </a>
           </li>
           <li>
